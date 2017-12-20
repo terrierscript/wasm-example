3be988d1d4bc35b30732290ce49a407a36a9e0ee
inuscript <inuscript@gmail.com>
2017-12-20 10:34:28 +0900

    node ver

--- node.js
+++ node.js
@@ -1,8 +1,10 @@
 const fs = require("fs")
 const wasm = "./public/hello.wasm"
+const buffer = fs.readFileSync(wasm)
 
-const buf = fs.readFileSync(wasm)
-
-const lib = Wasm.instantiateModule(toUint8Array(buf)).exports;
-
-console.log(lib)
+// const lib = Wasm.instantiateModule(toUint8Array(buf)).exports;
+const w = WebAssembly.compile(buffer)
+  .then( module => {
+    const instance = WebAssembly.Instance(module, {})
+    console.log(instance.exports.add(2,3))
+  })
