2643abcf847b037729b6016cc5ccc5893e64b698
inuscript <inuscript@gmail.com>
2017-12-20 14:08:26 +0900

    add

--- wasm/hello.ts
+++ wasm/hello.ts
@@ -1,13 +1,3 @@
 export function add(x: f64, y: f64): f64 {
   return x + y;
 }
-
-
-export function fib(n: f64) : f64 {
-  return n < 2 ? n : fib(n - 2) + fib(n - 1);
-}
-
-// function fibjs(n) {
-//   console.log(n)
-//   return n < 2 ? n : fibjs(n - 2) + fibjs(n - 1);
-// }
