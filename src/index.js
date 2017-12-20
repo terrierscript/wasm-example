1c4b62ec9e911dd4dfdc27575df9e5fdbe16302d
inuscript <inuscript@gmail.com>
2017-12-20 17:46:30 +0900

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 // import registerServiceWorker from './registerServiceWorker';
 // registerServiceWorker();
 
+const { load } = require("assemblyscript-loader")
+
+
+fetch('./bin.wasm')
+  .then(result => result.arrayBuffer())
+  .then( buf => load(buf) )
+  .then( module => {
+    const ex = module.exports
+    console.log(ex)
+
+    const result = ex.echo(3)
+    ex.ping()
+    document.querySelector("body").innerHTML = result
+  }).catch(e => {
+    console.warn(e)
+  })
--- wasm/bin.ts
+++ wasm/bin.ts
@@ -2,4 +2,6 @@ export function echo(val: f64): f64 {
   return val;
 }
 
-
+export function ping(): void{
+  console.log("pong")
+}
