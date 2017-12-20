f0ca1fd6c323b5ab90bc37fec292d3450052c76e
inuscript <inuscript@gmail.com>
2017-12-20 16:59:35 +0900

    add

--- node.js
+++ node.js
@@ -3,8 +3,16 @@ const wasm = "./public/hello.wasm"
 const buffer = fs.readFileSync(wasm)
 
 // const lib = Wasm.instantiateModule(toUint8Array(buf)).exports;
-const w = WebAssembly.compile(buffer)
-  .then( module => {
-    const instance = WebAssembly.Instance(module, {})
-    console.log(instance.exports.add(2,3))
-  })
+// const w = WebAssembly.compile(buffer)
+//   .then( module => {
+//     const instance = WebAssembly.Instance(module, {})
+//     console.log(instance.exports.add(2,3))
+//   })
+
+const { load } = require("assemblyscript-loader")
+
+load(wasm).then( module => {
+  console.log(module.exports.hello())
+  // console.log(instance.exports.add(2,3))
+
+})
